51aee313a6a96e32fdb49b6d49b86cea5f31c8b0
Ghannane Intissar <ghannaneintissar@gmail.com>
2025-07-31 13:48:48 +0100

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'add_shipment_screen.dart';
 import 'customer_form1.dart';
@@ -6,7 +8,25 @@ import 'login_screen.dart';
 
 
 
-void main() {
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  if(kIsWeb)
+    {
+     await Firebase.initializeApp(options: FirebaseOptions(apiKey: "AIzaSyDzY5E2MHClH9ruIyGxW6i_7hxVxi0y9Fc",
+          authDomain: "yanship-c3893.firebaseapp.com",
+          projectId: "yanship-c3893",
+          storageBucket: "yanship-c3893.firebasestorage.app",
+          messagingSenderId: "969048987321",
+          appId: "1:969048987321:web:211b0221f04f096a9a6d68"));
+
+    }else{
+    await Firebase.initializeApp();
+  }
+
+
+
+
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: LoginScreen(),
